db66ff284532000809c63178c665ac480dc12a2f
DariDavid8 <168194898+DariDavid8@users.noreply.github.com>
2024-04-27 02:05:02 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
-global.premium = ['94784506970']
-global.ownernomer = '94784506970'
-global.ownername = 'Achi Fernando'
+global.owner = ['628154807477']
+global.premium = ['6281291061314']
+global.ownernomer = '6283154807477'
+global.ownername = 'VINN'
 global.botname = '🎭ᴀʟᴘʜᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ🎭 v1.0.0'
 global.footer = ' ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
 global.ig = 'https://github.com/ALPHA-OFFICIAL-TEAM'
